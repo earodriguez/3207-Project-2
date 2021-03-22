943067fcb8af024e1860f7bbe523d8e6a2d93996
Erik Rodriguez <earodriguez1996@gmail.com>
2021-03-22 17:46:55 -0400

    Added headers to utility.h, porting over some code from previous attempt

--- myshell.c
+++ myshell.c
@@ -1,2 +1,96 @@
 //Erik Rodriguez
 //Main file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "utility.h"
+
+int main (int argc, char *argv[], char **envp){
+
+    char line[];
+    char * toks[]
+    int tokSize;
+
+    pid = -1;
+
+}
+
+/*
+Processes initialization, differentiates between interactive mode and
+batch/txt file reading mode.
+*/
+int init(){
+
+}
+
+void mysh_prompt(){
+    char PWD[PATH_MAX];
+    do {
+        if (getcwd(PWD, sizeof(PWD)) != NULL){
+            printf("%s/myshell> ", PWD);
+        }
+        else{
+            perror("Could not get current directory");
+        }
+    }
+}
+
+int mysh_cd(char *args[]){
+    if (args[1] == NULL){
+        fprintf(stderr, "mysh: expected argument to \"cd\"\n");
+    }
+    else {
+        if (chdir(args[1]) != 0){
+            perror("mysh");
+        }
+    }
+    return 1;
+}
+void mysh_clr(){
+    system("clear");
+}
+int mysh_dir(){
+}
+int mysh_environ(){
+}
+int mysh_echo(char *args[]){
+}
+int mysh_help(){
+}
+int mysh_pause(){
+}
+void mysh_quit(){
+exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utility.h
+++ utility.h
@@ -1,3 +1,18 @@
+
+//headers
+int main (int argc, char *argv[], char **envp);
+int init();
+void mysh_prompt();
+
+int mysh_cd(char *args[]);
+void mysh_clr();
+int mysh_dir();
+int mysh_environ();
+int mysh_echo(char *args[]);
+int mysh_help();
+int mysh_pause();
+void mysh_quit();
+
 static pid_t MYSH_PID;
 static pid_t MYSH_PGID;
 static int MYSH_INTRACTV
